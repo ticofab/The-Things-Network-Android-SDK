e232d236dccd8ef57ce2f5925e1de4aa934c56cd
Fabio Tiriticco <fabio.europe@gmail.com>
2015-09-25 23:43:51 +0200

    displaying packets

--- app/src/main/java/org/ttn/android/sample/MainActivity.java
+++ app/src/main/java/org/ttn/android/sample/MainActivity.java
@@ -3,67 +3,96 @@ package org.ttn.android.sample;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import org.ttn.android.sdk.Listener;
 import org.ttn.android.sdk.TTNClient;
 import org.ttn.android.sdk.domain.packet.Packet;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/*
+ * Copyright 2015 The Things Network
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by fabiotiriticco on 25/09/15.
+ *
+ */
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.packet_list) RecyclerView mPacketList;
+
     final TTNClient mTTNClient = new TTNClient();
+    final List<Packet> mPackets = new ArrayList<>();
+    final PacketAdapter mAdapter = new PacketAdapter(mPackets);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+
+        // setup recycler view to show packets
+        mPacketList.setLayoutManager(new LinearLayoutManager(this));
+        mPacketList.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mTTNClient.get(new Listener() {
-                    @Override
-                    public void onResult(List<Packet> packets) {
-                        Log.d("t", "got packets! " + packets.size());
-                    }
-
-                    @Override
-                    public void onError() {
-                        Log.d("t", "error!");
-                    }
-                });
+                refreshPackets();
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onStart() {
+        super.onStart();
+
+        // initial packets refresh
+        refreshPackets();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    /**
+     * Triggers a packet refresh from the APIs.
+     */
+    void refreshPackets() {
+        mTTNClient.get(new Listener() {
+            @Override
+            public void onResult(List<Packet> packets) {
+                mPackets.addAll(packets);
+                mAdapter.notifyDataSetChanged();
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+            @Override
+            public void onError() {
+                Toast.makeText(MainActivity.this, "Failed to retrieve packets", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/org/ttn/android/sample/PacketAdapter.java
@@ -0,0 +1,81 @@
+package org.ttn.android.sample;
+
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.ttn.android.sdk.domain.packet.Packet;
+
+import java.text.DateFormat;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/*
+ * Copyright 2015 The Things Network
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by fabiotiriticco on 25/09/15.
+ *
+ */
+
+public class PacketAdapter extends RecyclerView.Adapter<PacketAdapter.ViewHolder> {
+
+    List<Packet> mPackets;
+    DateFormat mDateFormatter = DateFormat.getDateInstance(DateFormat.FULL);
+
+    public PacketAdapter(List<Packet> packets) {
+        mPackets = packets;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_packet, parent, false);
+        return new ViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder vh, int position) {
+        Packet packet = mPackets.get(position);
+        vh.mDeviceId.setText(packet.getNodeEui());
+        vh.mTime.setText(mDateFormatter.format(packet.getTime().toDate()));
+
+        // Print the packet data. Try the most human-readable format.
+        String data = TextUtils.isEmpty(packet.getDataPlain()) ?
+                (TextUtils.isEmpty(packet.getData()) ?
+                        (TextUtils.isEmpty(packet.getDataRaw()) ?
+                                null : packet.getDataRaw()) : packet.getData()) : packet.getDataPlain();
+        vh.mData.setText(data);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPackets.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.packet_device_id) TextView mDeviceId;
+        @Bind(R.id.packet_time) TextView mTime;
+        @Bind(R.id.packet_data) TextView mData;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
